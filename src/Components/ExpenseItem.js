bea62804e757b736f3df8a8c301cb5ddd4c58dcb
VishuPathania <vishalpathania0003@gmail.com>
2023-05-28 22:39:15 +0530

    fourth commit smallercomps

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
   
-  //earlier we hard coded now we are getting values from props
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/> 
       <div className="expense-item__description">
         <h2> {props.title}</h2> 
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
-    //JSX code must only have 1 root element per return statement so wrapped it in another div
+  
   );
 }
 
