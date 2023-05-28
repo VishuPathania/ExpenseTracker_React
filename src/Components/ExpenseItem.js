91645368ee57598cf554913da1aefb475b2d3102
VishuPathania <vishalpathania0003@gmail.com>
2023-05-28 21:19:15 +0530

    third  commit multiplecomponents

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 4, 29);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 500.0;
-  const locationOfExpenditure = "Misc";
+function ExpenseItem(props) {
+  
+  //earlier we hard coded now we are getting values from props
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2> {expenseTitle}</h2>
-        <h2> {locationOfExpenditure} </h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2> {props.title}</h2> 
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
     //JSX code must only have 1 root element per return statement so wrapped it in another div
