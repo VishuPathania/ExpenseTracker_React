3980a07117383aaaeadd4f070a028713dc90bb05
VishuPathania <vishalpathania0003@gmail.com>
2023-05-31 22:35:29 +0530

    Delete Button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem =(props) => {
-  
+    const [amount, setAmount] = useState(props.amount) ;
+ 
+  const clickHandler= () => {
+    setAmount('0 $');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/> 
       <div className="expense-item__description">
         <h2> {props.title}</h2> 
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={clickHandler}>Delete Expense</button>
       </div>
     </Card>
   
